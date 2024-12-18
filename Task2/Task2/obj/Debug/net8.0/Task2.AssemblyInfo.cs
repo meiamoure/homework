577f76afcf91f8f7ca624d4112f4a3214c0e4bd9
meiamoure <masha992827@gmail.com>
2024-12-18 14:00:21 +0100

    Deleted unnecessary files

--- Task2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655e8de8fe6fa17022b7b314e2ea4fbbb4d052de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
